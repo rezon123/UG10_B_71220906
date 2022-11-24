45a95fd1cc72c3f8c503abbafd5593b48ae4847c
rezon123 <118422752+rezon123@users.noreply.github.com>
2022-11-24 14:22:02 +0700

    Add files via upload

--- soal 1.py
+++ soal 1.py
@@ -11,5 +11,11 @@ pilihan = int(input("masukkan pilihan anda: "))
 if pilihan == 1:
     alaslimas = int(input("masukkan panjang sisi alas limas: "))
     tinggilimas = int(input("masukkan tinggi limas: "))
-    vol_limas = alaslimas * alaslimas * tinggilimas
+    vol_limas = alaslimas * alaslimas * tinggilimas * 1/3
     print("volume limas tersebut adalah", vol_limas)
+    pilihan = int(input("masukkan pilihan anda: "))
+elif pilihan == 2:
+    pjarijaribola = int(input("masukkan panjang jari-jari bola: "))
+    vol_bola = (4/3 * pjarijaribola**3*3.14)
+    print("volume bola tersebut adalah", vol_bola)
+    pilihan = int(input("masukkan pilihan anda: "))
